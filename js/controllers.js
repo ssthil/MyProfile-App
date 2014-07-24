955dc387a833015a3d397f64ebe9d2e8bce09965
ssthil <ssthil@gmail.com>
2014-07-24 14:09:46 +0800

    experience content updated

--- js/controllers.js
+++ js/controllers.js
@@ -10,7 +10,15 @@ app.controller('AboutCtrl', function($scope){
 	}
 	];
 });
-app.controller('ProfileCtrl', function($scope){
+app.controller('ProfileCtrl', function($scope, $http){
+	//$scope.profilelinks = [];
+	$http.get("js/json/profile.json")
+		.success(function(data){ 
+		console.log("Success: I'm ", JSON.stringify(data[0]));
+		$scope.profilelinks = data;
+		})
+});
+/* app.controller('ProfileCtrl', function($scope){
 	$scope.profilelinks = [
 	{
 		title:'Online Profile',
@@ -33,7 +41,7 @@ app.controller('ProfileCtrl', function($scope){
 		link:'http://ssthil.blogspot.in/'
 	}
 	];
-});
+}); */
 
 app.controller('TechCtrl', function($scope){
 	$scope.technicalskills = [
@@ -62,12 +70,60 @@ app.controller('QualificationCtrl', function($scope){
 app.controller('ExpCtrl', function($scope){
 	$scope.experience= [
 	{
-		fname:"Emerio",
-		lname:"Globesoft",
-		gender:'Male',
-		dob:'15-06-1983',
-		marital_status:'Married',
-		nationality:'Indian'
+		cname:"Emerio Globesoft, Singapore",
+		experience:"from Jan 2013 to till",
+		role:'UI/UX Engineer - Front End Developer',
+		skills:'HTML, CSS, Javascript, Jquery, HTML5, CSS3, Photoshop, Bootstrap, PHP, AngularJS, Jquery-Mobile',
+		responsibility:'UI Design and integration'
+	},
+	{
+		cname:"Connex Info System, Bangalore, India",
+		experience:"from Oct 2011 to Jan 2013",
+		role:'Senior UI Engineer - Front End Developer',
+		skills:'Asp.net Visual Studio 2008 - 2010, JavaScript, CSS,HTML, MVC Framework, Photoshop, Jquery, HTML5, CSS3',
+		responsibility:'UI Design and integration'
+	},
+	{
+		cname:"Adodis Technologies, Bangalore, India",
+		experience:"from Jun 2010 to Oct 2011",
+		role:'Team Lead - Senior Web GUI Designer',
+		skills:'Adobe Photoshop CS3, HTML, CSS, Basic Jquery',
+		responsibility:'Magento ecommerce Themes Creation, Drupal Business Themes Creation, Quality Analyze, UI Design for Joomla Module, Android Apps Design'
+	},
+	{
+		cname:"Maxval-IP Ventures India (P) Ltd, Coimbatore, India",
+		experience:"from June 2010 to Oct 2010",
+		role:'Sr.Web and GUI Designer',
+		skills:'Adobe Photoshop CS3, Visual Studio 2008, HTML, CSS, Javascript, Silverlight',
+		responsibility:'UI Design for Web Application and Windows Application'
+	},
+	{
+		cname:"Banyan InfoTech, Coimbatore, India",
+		experience:"from Dec 2008 to June 2010",
+		role:'Team Lead / Senior Web Designer',
+		skills:'Adobe Photoshop CS3, Dreamweaver CS3, Flash CS3, HTML, CSS, Basic Php, Basic SEO, Basic Javascript',
+		responsibility:'Website Design, PSD to HTML conversion, Tabless Website, Flash Website, Flash Animation, Flash Presentation'
+	},
+	{
+		cname:"Global Vision, Coimbatore, India",
+		experience:"from March 2008 to Dec 2008",
+		role:'Web Designer',
+		skills:'Adobe Photoshop CS2, Dreamweaver 8, Flash 8, HTML, CSS',
+		responsibility:'Website Design, Logo Design, Brochure Design, Brand promotion'
+	},
+	{
+		cname:"Shalomtech Web Designing & Multimedia Institution, Coimbatore, India",
+		experience:"from Jan 2007 to Feb 2008",
+		role:'Web Designer and Multimedia Faculty',
+		skills:'Adobe Photoshop CS2, Dreamweaver 8, Flash 8, HTML, Corel Draw',
+		responsibility:'Website Design, Indoor Outdoor Advertisement Designs, Logo Design, Brochure Design, Brand promotion, Teach multimedia courses'
+	},
+	{
+		cname:"The Sign lab innovative sign maker, Coimbatore, India",
+		experience:"from Feb 2005 to Dec 2006",
+		role:'Graphic Designer',
+		skills:'Adobe Photoshop CS2, Corel Draw',
+		responsibility:'Indoor Outdoor Advertisement Designs, Logo Design, Brochure Design, Brand promotion'
 	}
 	];
 });
@@ -129,12 +185,12 @@ app.controller('ProfileListCtrl', function($scope){
 	{
 		title:'Career Experience',
 		desc:'While not always necessary, sometimes you need to put your DOM in a box. For those situations, try the panel component.',
-		id: 'career'	
+		id: 'experience'	
 	},
 	{
 		title:'Personal Details',
 		desc:'While not always necessary, sometimes you need to put your DOM in a box. For those situations, try the panel component.',
-		id: 'profile'
+		id: 'personal'
 	}
 	];
 });
